de4ca7a80ef7938a73e9d785519616bc1ba4aab0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-02 11:32:19 +1300

    Working on the inputs error message
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 const initialValues = { email: '', plan: '', phone: '', company: '' }
-
 const Form = () => {
 	const [values, setValues] = useState(initialValues)
 	const [error, setError] = useState('')
@@ -13,16 +12,13 @@ const Form = () => {
 	}
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		for (let name in values) {
-			if (values[name] === '') {
-				setError(`Please provide the ${name}`)
-				return
-			}
+		if (values.email && values.pack && values.phone && values.company) {
+			setError('')
+			const newRegister = { ...values, id: new Date().getTime().toString() }
+			setRegisters([...registers, newRegister])
+			setValues({ email: '', plan: '', phone: '', company: '' })
 		}
-		setError('')
-		const newRegister = { ...values, id: new Date().getTime().toString() }
-		setRegisters([...registers, newRegister])
-		setValues({ email: '', plan: '', phone: '', company: '' })
+		setError(`Please ${values[name]} is required!`)
 	}
 	return (
 		<>
@@ -36,23 +32,21 @@ const Form = () => {
 						value={values.email}
 						placeholder='email.email.com'
 						onChange={handleChange}
-						required
 					/>
-					{error && <span>{error}</span>}
+					{error && <span>Please enter a valid email address </span>}
 				</div>
 				<div className='form-group'>
-					<label htmlFor='plan'>Plan : </label>
+					<label htmlFor='pack'>Pack : </label>
 					<select
-						name='plan'
-						id='plan'
-						placeholder='Basic pack free'
-						value={values.plan}
+						name='pack'
+						id='pack'
+						value={values.pack}
 						onChange={handleChange}>
-						<option value='Basic pack free'>Basic pack free</option>
+						<option value='Basic pack free'>Basic pack Free</option>
 						<option value='Pro Pack $9.99'>Pro Pack $9.99</option>
 						<option value='Ultimate Pack $19.99'>Ultimate Pack $19.99</option>
 					</select>
-					{error && <span>{error}</span>}
+					{error && <span>Please select one of the options</span>}
 				</div>
 				<div className='form-group'>
 					<label htmlFor='phone'>Phone number :</label>
@@ -64,7 +58,7 @@ const Form = () => {
 						onChange={handleChange}
 						id='phone'
 					/>
-					{error && <span>{error}</span>}
+					{error && <span>Please enter a valid phone number</span>}
 				</div>
 				<div className='form-group'>
 					<label htmlFor='company'>Company :</label>
@@ -76,10 +70,24 @@ const Form = () => {
 						onChange={handleChange}
 						id='company'
 					/>
-					{error && <span>{error}</span>}
+					{error && <span>Please enter a company name</span>}
 				</div>
+
 				<button className='submit'>Send</button>
 			</form>
+			<div>
+				{registers.map((entry) => {
+					const { id, company, phone, pack, email } = entry
+					return (
+						<div key={id}>
+							<h4>{company}</h4>
+							<h4>{email}</h4>
+							<h4>{phone}</h4>
+							<h4>{pack}</h4>
+						</div>
+					)
+				})}
+			</div>
 		</>
 	)
 }
