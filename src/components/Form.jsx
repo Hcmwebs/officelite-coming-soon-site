1f098e96f4edc060357865775dd4a62c6a4f2e65
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-01 23:25:51 +1300

    added value and handle change to the inputs
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { formInputs } from '../data/Data'
+import React, {useState} from 'react'
 
 const Form = () => {
-	const handleChange = (e) => {}
+	const [value, setValue] = useState({
+		email: '',
+		select: '',
+		phone: '',
+		company: '',
+	})
+	const handleChange = (e) => {
+		const name = e.target.name
+		const value = e.target.value
+		setValue({...value, [name]:value})
+		console.log(name,value)
+	}
 	const handleSubmit = (e) => {
 		e.preventDefault()
 	}
@@ -11,12 +21,12 @@ const Form = () => {
 			<form onSubmit={handleSubmit}>
 				<div className='form-group'>
 					<label htmlFor='email'>Email</label>
-					<input type='email' placeholder='email@email.com' />
+					<input type='email' placeholder= 'email@email.com' name ='email' value={value} onChange = {handleChange} id ='email'/>
 					<span>Please enter a valid email address</span>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='select'>Select</label>
-					<select name='pack' placeholder='Basic pack free'>
+					<select name='pack' placeholder='Basic pack free'value={value} onChange = {handleChange} id = 'select'>
 						<option>Basic pack free</option>
 						<option>Pro Pack $9.99</option>
 						<option>Ultimate Pack $19.99</option>
@@ -25,12 +35,12 @@ const Form = () => {
 				</div>
 				<div className='form-group'>
 					<label htmlFor='phone'>Phone number :</label>
-					<input type='email' placeholder= {parseInt('+1234566789')} />
+					<input type='number' placeholder='+1234566789' name='phone' value={value} onChange = {handleChange} id='phone'/>
 					<span>Please enter a valid phone number</span>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='company'>Company</label>
-					<input type='text' placeholder='Company name' />
+					<input type='text' placeholder='Company name' value={value} onChange = {handleChange} id ='company'/>
 					<span>Please enter your company name</span>
 				</div>
 			</form>
