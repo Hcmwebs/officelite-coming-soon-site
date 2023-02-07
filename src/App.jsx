5630027b4e23769dda7a4d0111940b40d1978827
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-08 10:53:41 +1300

    removed trial components
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,6 @@ const App = () => {
 				<Route path ='SignUp' element={<SignUp/>}/>
 				<Route path ='*' element={<Error/>}/>
 			</Routes>
-			<h1>we are coming soon</h1>
-			<Timer duration={30 * 24 * 60 * 60 * 1000} />
-			<Form />
 		</BrowserRouter>
 	)
 }
