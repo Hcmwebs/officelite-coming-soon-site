1ba8f998af30cf7013512035ec913649f4f6307e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-08 17:16:55 +1300

    uodated the timer component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import { getFormattedTime } from '../utils/Helper'
+import styled from 'styled-components'
 
 const Timer = ({ duration }) => {
 	const [time, setTime] = useState(duration)
+	const getFormattedTime = (milliseconds) => {
+		let totalSeconds = parseInt(Math.floor(milliseconds / 1000))
+		let totalMinutes = parseInt(Math.floor(totalSeconds / 60))
+		let totalHours = parseInt(Math.floor(totalMinutes / 60))
+		let days = parseInt(Math.floor(totalHours / 24))
+
+		let secs = parseInt(totalSeconds % 60)
+		let mins = parseInt(totalMinutes % 60)
+		let hrs = parseInt(totalHours % 24)
+
+		return (
+			<div
+				className='timer'
+				style={{ display: 'flex', gap: 10, marginBottom: 24 }}>
+				<div>
+					{days} <span> days</span>
+				</div>
+				:
+				<div>
+					{hrs} <span>hours</span>
+				</div>
+				:
+				<div>
+					{mins} <span>mins</span>
+				</div>
+				:
+				<div>
+					{secs} <span>secs</span>
+				</div>
+			</div>
+		)
+	}
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -11,5 +43,17 @@ const Timer = ({ duration }) => {
 	}, [time])
 	return <div>{getFormattedTime(time)}</div>
 }
+const Wrapper = styled.div`
+display: grid;
+place-items: center;
+padding-block: 1.5rem;
+
+.timer{
+	display: flex;
+	gap: 10px;
+
+}
+
+`
 
 export default Timer
