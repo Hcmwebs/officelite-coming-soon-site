cbadc2f9891107429f1fb2d787f2e6ac35ed85db
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-09 11:37:08 +1300

    added GlobalStyles to the project
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SharedLayout, Home, SignUp, Error } from './pages'
+import { GlobalStyles } from './styles'
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<SharedLayout />}>
-					<Route index element = {<Home />}/>
-					<Route path='signup' element={<SignUp />} />
-					<Route path='*' element={<Error />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<SharedLayout />}>
+						<Route index element={<Home />} />
+						<Route path='signup' element={<SignUp />} />
+						<Route path='*' element={<Error />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
 	)
 }
 
