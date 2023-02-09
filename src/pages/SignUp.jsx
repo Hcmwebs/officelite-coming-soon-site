58da37562b16f89b9d86a9ca60bfc38d1dd4cf58
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-09 22:46:33 +1300

    added the page content
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Form, Timer } from '../components'
-import { timerDuration } from '../data/Data'
+import { signUpHero, timerDuration } from '../data/Data'
 
 const SignUp = () => {
-  return (
-    <div>
-      <h2>Please sign up here!</h2>
-      <Timer duration ={timerDuration} />
-      <Form />
-    </div>
-  )
+	const { title, body } = signUpHero
+	return (
+		<div>
+			<div className='heroBody'>
+				<h2>{title}</h2>
+				<p>{body}</p>
+			</div>
+			<Timer duration={timerDuration} />
+			<Form />
+		</div>
+	)
 }
 
 export default SignUp
