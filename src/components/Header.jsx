0f32c32064aca9c10307cefcd959a79195fe9099
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-09 22:33:57 +1300

    imported and rendered the hero content from data
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { homeHero } from '../data/Data'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+	const { title, body } = homeHero
 	return (
 		<>
 			<div>
 				<img src='/illustration-home-charts.svg' alt='illustration Chart' />
 				<div className='heroBody'>
-					<h1>  A simple solution to complex tasks is coming soon</h1>
+					<h1>{title}</h1>
+					<p>{body}</p>
 				</div>
+				<Link to ='/signup'>Get Started</Link>
 			</div>
 		</>
 	)
