6158dc7074248bc0df6ae978106585e037bca211
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-09 19:44:40 +1300

    removed comntent due to repetition
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
-import { Timer } from '../components'
+import { Link } from 'react-router-dom'
+import { Header, MainContent, Footer, Timer } from '../components'
 import { timerDuration } from '../data/Data'
 
 const Home = () => {
 	return (
 		<div>
-			<h1>Welcome Home</h1>
+			<MainContent />
+
+			{/* <h1>Welcome Home</h1>
 			<Timer duration={timerDuration} />
-			<Link to='signup'>Let's get Started!</Link>
+			<Link to='signup'>Let's get Started!</Link> */}
 		</div>
 	)
 }
