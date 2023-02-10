662af4d3e42fb56d91cc71c03643713c55545358
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-10 14:31:51 +1300

    modified the content of the compo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { timerDuration } from '../data/Data'
+import { Timer } from './index'
 
 const Footer = () => {
-  return <div>
-    <h2>footer goes here</h2>
-  </div>
+	return (
+		<Wrapper>
+			<Timer duration={timerDuration} />
+			<Link to='/signup'> Get Started</Link>
+		</Wrapper>
+	)
 }
 
-export default Footer;
+const Wrapper = styled.footer`
+	min-height: 30vh;
+  padding-top: 4rem;
+	background-color: var(--black);
+`
+
+export default Footer
