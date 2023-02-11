536a5941ad12b29d652669a93738a3236f315a99
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-11 21:01:21 +1300

    added styling for the container
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledContainer.jsx
+++ src/styles/StyledContainer.jsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 
 const StyledContainer = styled.div`
 	width: min(100%, 1440px);
-  
+	padding-inline: 1.5rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: column;
 `
 
 export default StyledContainer
