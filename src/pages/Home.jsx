9ff4c395e28beeb22cdaafdb2a26a810fc230ae6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-11 19:15:24 +1300

    deleted unnecessary test content
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Header, MainContent, Footer} from '../components'
-
+import { Header, MainContent, Footer } from '../components'
 
 const Home = () => {
 	return (
@@ -9,10 +8,6 @@ const Home = () => {
 			<Header />
 			<MainContent />
 			<Footer />
-
-			{/* <h1>Welcome Home</h1>
-			<Timer duration={timerDuration} />
-			<Link to='signup'>Let's get Started!</Link> */}
 		</div>
 	)
 }
