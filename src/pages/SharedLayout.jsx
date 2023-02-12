b1b1c87d721cb35274a76783d07f2b6b4ee629c7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-12 13:42:56 +1300

    rendered the logo int he sharedlayout
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '../components'
+import { Logo } from '../components'
 
 const SharedLayout = () => {
 	return (
 		<>
-			<Navbar />
+			<Logo />
 			<Outlet />
 		</>
 	)
