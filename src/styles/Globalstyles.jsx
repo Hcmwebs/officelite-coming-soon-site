b4367dff3f020d6188e7f0f4569fd31391567f60
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-13 12:52:58 +1300

    sty;led all the variants of the button

--- src/styles/Globalstyles.jsx
+++ src/styles/Globalstyles.jsx
@@ -6,7 +6,9 @@ const GlobalStyles = createGlobalStyle`
   --white-1: rgba(255, 255, 255, 1.0);
   --white-2:rgba(242, 242, 242, 1.0);
   --white-3:rgba(250, 250, 250, 1.0);
-  --blue: rgba(81, 117, 255, 1.0);
+  --blue-1: rgba(81, 117, 255, 1.0);
+  --blue-2: rgba(81, 117, 255, 0.65);
+  --blue-3: rgba(81, 117, 255, 0.75);
   --lightBlue: rgba(130,156,255,1.0);
   --black: rgba(37,41,58,1.0);
   --lightBlack: rgba(51,57,80,1.0);
@@ -80,13 +82,38 @@ a{
 }
 
 .btn{
-  padding: 0.75rem 1.5rem;
+  padding-inline:0.75rem 1.5rem;
   border: 1px solid var(--white-2);
   display: inline-block;
+  border-radius: 1.75rem;
+  transition: var(--transition-2);
 
   &.primary{
-    background-color:var(--blue);
-    color:var(--white-2);
+    background-color:var(--blue-1);
+    color:var(--white-1);
+
+    &:hover{
+    background-color:var(--lightBlue);
+    color:var(--white-1);
+    }
+  }
+  &.secondary{
+    background-color:var(--blue-2);
+    color:var(--blue-1);
+
+    &:hover{
+    background-color:var(--blue-3);
+    color:var(--blue-1);
+    }
+  }
+  &.tertiary{
+    background-color:var(--white-1);
+    color:var(--blue-1);
+
+    &:hover{
+    background-color:var(--white-1);
+    color:var(--lightBlue);
+    }
   }
 
 
