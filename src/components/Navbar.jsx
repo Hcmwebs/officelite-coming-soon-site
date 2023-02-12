b2bea63f57aa3b4840885159b7a8ed37c395b66e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-12 14:46:39 +1300

    removed the Logo Compo from the Navbar Compo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Link,NavLink } from 'react-router-dom'
-import {Logo} from './index'
+import { Link, NavLink } from 'react-router-dom'
+import { Logo } from './index'
 
 const Navbar = () => {
 	return (
 		<div>
-			<Logo />
 			<nav>
-				<NavLink to='/' style ={({isActive}) =>{return {color:isActive? 'salmon': 'blue'}}}> Home</NavLink>
-				<NavLink to='/signup' style ={({isActive}) =>{return {color:isActive? 'salmon': 'blue'}}}>Sign up</NavLink>
+				<NavLink
+					to='/'
+					style={({ isActive }) => {
+						return { color: isActive ? 'salmon' : 'blue' }
+					}}>
+					{' '}
+					Home
+				</NavLink>
+				<NavLink
+					to='/signup'
+					style={({ isActive }) => {
+						return { color: isActive ? 'salmon' : 'blue' }
+					}}>
+					Sign up
+				</NavLink>
 			</nav>
 		</div>
 	)
