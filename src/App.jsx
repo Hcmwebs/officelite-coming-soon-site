02f01e68c087346474dd124a188ce41eed0701ed
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-14 11:03:33 +1300

    moved the container to the main app
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SharedLayout, Home, SignUp, Error } from './pages'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, StyledContainer } from './styles'
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<SharedLayout />}>
-						<Route index element={<Home />} />
-						<Route path='signup' element={<SignUp />} />
-						<Route path='*' element={<Error />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<StyledContainer>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/' element={<SharedLayout />}>
+							<Route index element={<Home />} />
+							<Route path='signup' element={<SignUp />} />
+							<Route path='*' element={<Error />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</StyledContainer>
 		</>
 	)
 }
