7ff2d78d11f76c777c3221bc6dca61f1f8300003
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 11:58:56 +1300

    refactored the code
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledContainer.jsx
+++ src/styles/StyledContainer.jsx
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 const StyledContainer = styled.div`
 	width: min(100%, 1440px);
 	padding-inline: 1.5rem;
+	padding-bottom: ${({ footer }) => (footer ? '6.25rem' : '0rem')};
 	display: grid;
 	place-items: center;
 	position: relative;
-	background-color: ${({ header }) =>
-		header ? 'var(--white-3)' : 'var(--black)'};
+	background-color: ${({ footer }) =>
+		footer ? 'var(--black)' : 'var(--white-3)'};
 `
 
 export default StyledContainer
