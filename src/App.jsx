4ef167df11975ad45f2d7e89437591bd634be8f1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 11:11:50 +1300

    removed the container from the App compo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SharedLayout, Home, SignUp, Error } from './pages'
-import { GlobalStyles, StyledContainer } from './styles'
+import { GlobalStyles} from './styles'
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<StyledContainer>
-				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<SharedLayout />}>
-							<Route index element={<Home />} />
-							<Route path='signup' element={<SignUp />} />
-							<Route path='*' element={<Error />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</StyledContainer>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<SharedLayout />}>
+						<Route index element={<Home />} />
+						<Route path='signup' element={<SignUp />} />
+						<Route path='*' element={<Error />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	)
 }
