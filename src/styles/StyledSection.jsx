bfc04cabfb2de894749612d7211c654903d2cd6b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 13:51:29 +1300

    added the padding
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledSection.jsx
+++ src/styles/StyledSection.jsx
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 const StyledSection = styled.section`
 	min-height: 50vh;
+	padding-block: 3rem;
+	padding-inline: 1.5rem;
 	display: grid;
 	place-items: center;
 	grid-template-columns: 1fr;
-  gap: 2rem;
+	gap: 2rem;
 `
 
 export default StyledSection
