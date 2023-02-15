972446b03dcb1e7f57fa6c882a8b826b3067d226
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-16 12:38:41 +1300

    styled the card
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledCard.jsx
+++ src/styles/StyledCard.jsx
@@ -1,14 +1,82 @@
 import styled from 'styled-components'
 
 const StyledCard = styled.div`
-	width: min(100%, 100%);
-  min-height: 500px;
+	width: min(100%, 688px);
+	min-height: 500px;
+	padding-block: 2.5rem;
 	padding-inline: 1.5rem;
-	display: grid;
-	place-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: column;
+	gap: 2rem;
 	box-shadow: var(boxShadow-1);
-	border-radius: var(--borderRadius);
-	background-color: ${({className}) => className ==='pro'? 'var(--blue-1)': 'var(--white-1)' };
+	border-radius: var(--borderRadius-2);
+	background-color: ${({ className }) =>
+		className === 'pro' ? 'var(--blue-1)' : 'var(--white-1)'};
+	@media (min-width: 768px) {
+		min-height: 320px;
+		flex-direction: row;
+		position: relative;
+		padding-right: 4.25rem;
+		padding-left: 3rem;
+		.btn {
+			position: absolute;
+			bottom: 2rem;
+		}
+	}
+	@media (min-width: 1440px) {
+		width: min(100%, 350px);
+		min-height: 500px;
+		flex-direction: column;
+		padding-right: 1.5rem;
+		padding-left: 1.5rem;
+
+		.btn {
+			position: static;
+		}
+	}
+
+	.cardHeader {
+	}
+	h2 {
+		font-size: 1.25rem;
+		font-weight: var(--bold);
+		line-height: 1.75rem;
+		text-align: center;
+		color: ${({ className }) =>
+			className === 'pro' ? 'var(--white-1)' : 'var(--black)'};
+		@media (min-width: 768px) {
+			align-self: start;
+		}
+	}
+	.price {
+		display: grid;
+		place-items: center;
+		gap: 0.5rem;
+		padding-block: 0.5rem;
+	}
+	h3 {
+		font-size: 3.5rem;
+		font-weight: var(--bold);
+		line-height: 4rem;
+		text-align: center;
+		color: ${({ className }) =>
+			className === 'pro' ? 'var(--white-1)' : 'var(--black)'};
+	}
+	p {
+		font-size: 1rem;
+		font-weight: var(--regular);
+		line-height: 1.75rem;
+		text-align: center;
+		color: ${({ className }) =>
+			className === 'pro' ? 'var(--white-1)' : 'var(--black)'};
+	}
+	.cardBody {
+		display: grid;
+		place-items: center;
+		gap: 1rem;
+	}
 `
 
 export default StyledCard
