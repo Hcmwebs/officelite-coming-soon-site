8671ea1d535c8c9dca15631e9aae78e411acc43c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 13:50:57 +1300

    rendered the contenct from data
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { StyledContainer, StyledMainContent, StyledSection } from '../styles'
+import {
+	StyledContainer,
+	StyledMainContent,
+	StyledSection,
+} from '../styles'
+import { pricing } from '../data/Data'
+import Card from './index'
 
 const MainContent = () => {
 	return (
 		<StyledContainer>
 			<StyledMainContent>
 				<StyledSection>
-					{}
-					<div>Price card components go here</div>
-					<div>Price card components go here</div>
-					<div>Price card components go here</div>
+					{pricing.map((card) => {
+						return <Card key={card.id} {...card} />
+					})}
 				</StyledSection>
 			</StyledMainContent>
 		</StyledContainer>
