12b35a674799eec41d0840e4b6713266d2071064
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-16 12:29:11 +1300

    added content to the card
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,14 +15,18 @@ const Card = ({
 }) => {
 	return (
 		<StyledCard className={className}>
-			<h2>{title}</h2>
-			<div>
-				<h3>{price}</h3>
-				<p>{billing}</p>
+			<div className='cardHeader'>
+				<h2>{title}</h2>
+				<div className='price'>
+					<h3>{price}</h3>
+					<p>{billing}</p>
+				</div>
 			</div>
-			<p>{info}</p>
-			<p>{storage}</p>
-			<p>{perks}</p>
+			<div className='cardBody'>
+				<p>{info}</p>
+				<p>{storage}</p>
+				<p>{perks}</p>
+			</div>
 			<Link to='/signup' className={`${btnClassName} btn secondary`}>
 				Try for Free
 			</Link>
