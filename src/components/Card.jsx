2f74eb95c70900da13c978985a3d69e91bbb3cbb
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 13:49:19 +1300

    added, indexed nad rendered compo

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledCard } from '../styles'
+
+const Card = ({ id, title, price, billing, info, storage, perks }) => {
+	return (
+		<StyledCard>
+			<h2>{title}</h2>
+      <h3>{price}</h3>
+      
+		</StyledCard>
+	)
+}
+
+export default Card
---
+++ src/styles/StyledCard.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+	width: min(100%, 300px);
+	padding-inline: 1.5rem;
+	display: grid;
+	place-items: center;
+	border: 1px solid #ccc;
+	border-radius: var(--borderRadius);
+`
+
+export default StyledCard
