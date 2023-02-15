75bb59bf88ec0d2fcca323725afc539f17810072
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-15 13:50:28 +1300

    whitespace typo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledHeader.jsx
+++ src/styles/StyledHeader.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 
 const StyledHeader = styled.header`
-
 	display: flex;
 	align-items: space-between;
 	justify-content: center;
 	flex-direction: column;
-  
 `
 export default StyledHeader
