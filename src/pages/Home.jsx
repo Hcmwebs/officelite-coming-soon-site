e107dd1c882a927e3d2b157dfe3d9ad756d07a4c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-18 21:38:11 +1300

    added a container ro the page
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Header, MainContent, Footer } from '../components'
+import { StyledContainer } from '../styles'
 
 const Home = () => {
 	return (
-		<>
+		<StyledContainer>
 			<Header />
 			<MainContent />
 			<Footer />
-		</>
+		</StyledContainer>
 	)
 }
 
