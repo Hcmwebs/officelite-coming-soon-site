a864d20885227942f22c2b0d3684dd87069c813a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-21 23:02:06 +1300

    added the background img
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledFooter.jsx
+++ src/styles/StyledFooter.jsx
@@ -10,7 +10,8 @@ const StyledFooter = styled.footer`
 	align-items: center;
 	flex-direction: column;
 	gap: 2.5rem;
-
+	position: relative;
+	overflow: hidden;
 	@media (min-width: 768px) {
 		padding-top: 31.63rem;
 	}
@@ -18,6 +19,27 @@ const StyledFooter = styled.footer`
 		padding-top: 15.63rem;
 		justify-content: space-between;
 		flex-direction: row;
+	}
+
+	&::before {
+		content: '';
+		width: 100%;
+		height: 1248px;
+		position: absolute;
+		top: 5%;
+		background: url('/public/bg-pattern-home-footer.svg') no-repeat center
+			center;
+		z-index: 9;
+
+		@media (min-width: 768px) {
+			height: 986px;
+			top: 30%;
+		}
+		@media (min-width: 1440px) {
+			height: 857px;
+			top: 0%;
+			left: -30%;
+		}
 	}
 `
 
