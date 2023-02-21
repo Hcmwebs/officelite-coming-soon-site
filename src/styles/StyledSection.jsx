21194895777a4026705c65504957f98e34ea567d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-21 22:04:26 +1300

    removed the padding-inline
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledSection.jsx
+++ src/styles/StyledSection.jsx
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
 
 const StyledSection = styled.section`
-	width: min(100%,100%);
-	padding-inline: 1.5rem;
+	width: min(100%, 100%);
 	display: grid;
 	place-items: center;
 	grid-template-columns: 1fr;
 	gap: 2rem;
 
 	@media (min-width: 1440px) {
-		width:min(100%, 1110px);
+		width: min(100%, 1110px);
 		grid-template-columns: repeat(3, 1fr);
 	}
 `
