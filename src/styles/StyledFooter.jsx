292564ee8f3acc27644949d84d31167fbe4953e0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-21 22:02:58 +1300

    changed to display flex and set up for all screens
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledFooter.jsx
+++ src/styles/StyledFooter.jsx
@@ -5,16 +5,19 @@ const StyledFooter = styled.footer`
 	min-height: 30vh;
 	padding-bottom: 2.5rem;
 	padding-top: 48.9rem;
-	display: grid;
-	place-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 	gap: 2.5rem;
 
 	@media (min-width: 768px) {
 		padding-top: 31.63rem;
-
 	}
 	@media (min-width: 1440px) {
 		padding-top: 15.63rem;
+		justify-content: space-between;
+		flex-direction: row;
 	}
 `
 
