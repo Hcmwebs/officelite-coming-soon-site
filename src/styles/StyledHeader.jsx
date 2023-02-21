d6a3fa6ec89e01ac1ff990ccd728f548539764f4
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-21 23:49:07 +1300

    whitespace typo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledHeader.jsx
+++ src/styles/StyledHeader.jsx
@@ -2,18 +2,19 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
 	width: 100%;
-	padding-bottom:48.9rem;
+	padding-bottom: 48.9rem;
 	display: flex;
 	align-items: space-between;
 	justify-content: center;
 	flex-direction: column;
 
-@media (min-width: 768px){
-	padding-bottom:31.63rem;
-}
-@media (min-width: 1440px){
-	padding-bottom:15.63rem;
-}
+
+	@media (min-width: 768px) {
+		padding-bottom: 31.63rem;
+	}
+	@media (min-width: 1440px) {
+		padding-bottom: 15.63rem;
+	}
 
 `
 export default StyledHeader
