428d125e107d5a40db07b372bde9fe6b8579861c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-23 11:32:05 +1300

    set up som media queries
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledSection.jsx
+++ src/styles/StyledSection.jsx
@@ -6,9 +6,16 @@ const StyledSection = styled.section`
 	place-items: center;
 	grid-template-columns: 1fr;
 	gap: 2rem;
+	position: absolute;
+	top: -48.9rem;
+	z-index: 9;
 
+	@media (min-width: 768px) {
+		top: -31.63rem;
+	}
 	@media (min-width: 1440px) {
 		width: min(100%, 1110px);
+		top: -15.63rem;
 		grid-template-columns: repeat(3, 1fr);
 	}
 `
