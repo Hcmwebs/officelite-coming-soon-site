407ead3987d882be27c390f24ecd46b6a3478277
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-23 12:41:43 +1300

    added footer class for the pattern
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledPattern.jsx
+++ src/styles/StyledPattern.jsx
@@ -1,22 +1,43 @@
 import styled from 'styled-components'
 
 const StyledPattern = styled.div`
+	position: absolute;
+
 	&.header {
-		position: absolute;
-		width: 100%;
+		width: 458px;
 		height: 458px;
 		top: -20%;
 
-    @media (min-width: 768px) {
-      height: 666px;
-      top:-30%;
-      right: -50%;
-    }
-    @media (min-width: 1440px) {
-      height: 1134px;
-      top:-65%;
-      right: -40%;
-    }
+		@media (min-width: 768px) {
+			width: 666px;
+			height: 666px;
+			top: -30%;
+			right: -50%;
+		}
+		@media (min-width: 1440px) {
+			width: 1134px;
+			height: 1134px;
+			top: -65%;
+			right: -40%;
+		}
+	}
+
+	&.footer {
+		width: 1202px;
+		height: 1202px;
+		position: absolute;
+		top: 9%;
+		@media (min-width: 768px) {
+			width: 1454px;
+			height: 1454px;
+			top: -3%;
+		}
+		@media (min-width: 1440px) {
+			width: 2036px;
+			height: 2036px;
+			top: -115%;
+			left: -50%;
+		}
 	}
 `
 export default StyledPattern
