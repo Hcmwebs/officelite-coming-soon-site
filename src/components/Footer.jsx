df1a1fd39ec0dc1b726dbd155b7359efb0e1f18f
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-23 12:28:15 +1300

    removed container import
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { timerDuration } from '../data/Data'
-import { StyledContainer, StyledFooter } from '../styles'
-import { Timer } from './index'
+import { StyledFooter } from '../styles'
+import { Pattern, Timer } from './index'
 
 const Footer = () => {
 	return (
 		<StyledFooter className='sectionCenter'>
+			<Pattern title='footer' image='/bg-pattern-home-footer.svg' />
 			<Timer duration={timerDuration} />
 			<Link to='/signup' className='btn primary'>
 				Get Started
