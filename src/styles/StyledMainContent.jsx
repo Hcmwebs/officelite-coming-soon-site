4d294971fac61375820a9df971dbf4dde0740e8b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-23 11:31:32 +1300

    removed unnecessary breakpoints
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledMainContent.jsx
+++ src/styles/StyledMainContent.jsx
@@ -4,17 +4,7 @@ const StyledMainContent = styled.main`
 	width: 100%;
 	display: grid;
 	place-items: center;
-	/* background-color: cyan; */
-	position: absolute;
-	top: -48.9rem;
-	z-index: 9;
-
-	@media (min-width: 768px) {
-		top: -31.63rem;
-	}
-	@media (min-width: 1440px) {
-		top: -15.63rem;
-	}
+	position: relative;
 `
 
 export default StyledMainContent
