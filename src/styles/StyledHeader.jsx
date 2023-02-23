b327c09de616eb6a74dbccab626cb358810bde5d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-23 16:19:26 +1300

    added a sign up class
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledHeader.jsx
+++ src/styles/StyledHeader.jsx
@@ -2,20 +2,24 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
 	width: 100%;
-	padding-bottom: 48.9rem;
-	display: flex;
-	align-items: space-between;
-	justify-content: center;
-	flex-direction: column;
-	position: relative;
+	&.homePage {
+		padding-bottom: 48.9rem;
+		display: flex;
+		align-items: space-between;
+		justify-content: center;
+		flex-direction: column;
+		position: relative;
 
-
-	@media (min-width: 768px) {
-		padding-bottom: 31.63rem;
+		@media (min-width: 768px) {
+			padding-bottom: 31.63rem;
+		}
+		@media (min-width: 1440px) {
+			padding-bottom: 15.63rem;
+		}
 	}
-	@media (min-width: 1440px) {
-		padding-bottom: 15.63rem;
-	}
 
+	.signUpPage{
+		
+	}
 `
 export default StyledHeader
