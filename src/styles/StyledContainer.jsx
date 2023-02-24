7887f4a1834f788c63b10d9a51477afbc1595497
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-02-24 13:07:41 +1300

    replaced the StyledContainer
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/StyledContainer.jsx
+++ src/styles/StyledContainer.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 
 const StyledContainer = styled.div`
-	width: min(100%, 1440px);
+	width: min-content(100%, 1440px);
 	margin: 0 auto;
-	display: grid;
-	place-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: column;
 	position: relative;
 	background-color: var(--white-3);
 	overflow: hidden;
 `
-
 export default StyledContainer
