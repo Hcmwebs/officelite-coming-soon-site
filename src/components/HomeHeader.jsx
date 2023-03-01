e147f8c58cf6b9c033fd095394ca7e6c71e83d98
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-01 14:37:38 +1300

    fixed the classes
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -4,10 +4,11 @@ import { HomeHero, Pattern } from './index'
 import { StyledHeader } from '../styles'
 const HomeHeader = () => {
 	return (
-		<StyledHeader className={'homePage sectionCenter'}>
+		<StyledHeader className='homePage sectionCenter'>
 			<Pattern title='homePage' image='/bg-pattern-home-header.svg' />
-			<HomeHero page= 'homePage' />
+			<HomeHero page='homePage' />
 		</StyledHeader>
-	)}
+	)
+}
 
 export default HomeHeader
